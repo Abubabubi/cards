2ad846f6c8474808d93a7dbb3084863a43393c86
Anton <anton.kom@hotmail.com>
2018-07-11 23:54:04 +0300

    Learn the bad way of comprehensing lists

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,9 +1,22 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three", "Four"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+      "Nine" , "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Hearths","Clubs", "Diamonds"]
+
+    cards = for suit <- suits do
+      for value <- values do
+        "#{value} of #{suit}"
+      end
+    end
+    List.flatten(cards)
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
